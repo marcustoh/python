d92d8f0ce20781f19b4c03957f86e9598ac74564
marcustoh <64060180+marcustoh@users.noreply.github.com>
2020-04-21 15:03:00 +0800

    Assignment 11

---
+++ Marcus Assignment 11.py
@@ -0,0 +1,25 @@
+Python 3.7.6 (tags/v3.7.6:43364a7ae0, Dec 19 2019, 00:42:30) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> hall=100
+>>> hall
+100
+>>> attend=['a','b','c','d','e','f','g','h','i']
+>>> hall-len(attend)
+91
+>>> n=[1,2,3,4,5,6,7,8,9,10]
+>>> for every in n:
+	loop=loop*every
+
+	
+Traceback (most recent call last):
+  File "<pyshell#6>", line 2, in <module>
+    loop=loop*every
+NameError: name 'loop' is not defined
+>>> loop=1
+>>> for every in n:
+	loop=loop*every
+
+	
+>>> loop
+3628800
+>>> 
